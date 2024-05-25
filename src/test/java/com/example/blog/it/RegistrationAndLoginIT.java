fe476b7fbb80dc16d9c58c506190f5abe41af2be
km-yoshihiro <nockupstream1206@gmail.com>
2024-05-25 16:22:59 +0900

    スプリングセキュリティ

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,7 +17,21 @@ public class RegistrationAndLoginIT {
 
     @Test
     public void integrationTest() {
+            //## Arrange ##
 
-        assertThat(webTestClient).isNotNull();
+            //## Act ##
+            var responseSpec = webTestClient.get().uri("/").exchange();
+
+            //## Assert ##
+            var response = responseSpec.returnResult(String.class);
+            var xsrfTokenOpt = Optional.ofNullable(response.getResponseCookies().getFirst("XSRF-TOKEN"));
+            //optionalでポストマンで表示されるようなXSRF-TOKENの長いコードが戻り値として挿入される
+            responseSpec.expectStatus().isNoContent();
+            assertThat(xsrfTokenOpt)
+                    .isPresent()//Optional<null>の時失敗する
+                    .hasValueSatisfying(xsrfTokenCookie ->
+                            assertThat(xsrfTokenCookie.getValue()).isNotBlank()
+                    ); //Optional<XSRF-TOKEN=xxxxx>
+
     }
 }
